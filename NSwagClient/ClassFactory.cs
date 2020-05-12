f3d3a6fd524f34264478b04175e7e423b829daf5
ulswww <ulswww@gmail.com>
2020-05-12 21:21:09 +0800

    Add files via upload

---
+++ NSwagClient/ClassFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace API
+{
+    static  class ClassFactory
+    {
+        static  IClient client;
+        public static IClient Get()
+        {
+            if(client == null)
+            {
+                client = new ClientImpl("http://shambhala.dev.clolink.cn/");
+            }
+
+            return client;
+        }
+    }
+}
