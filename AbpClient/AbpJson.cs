d1e98fa678177ca99a92e6fbb371622c5becd095
ulswww <ulswww@gmail.com>
2020-05-12 21:22:54 +0800

    Add files via upload

---
+++ AbpClient/AbpJson.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API
+{
+    /// <summary>
+    ///  result_ = Newtonsoft.Json.JsonConvert.DeserializeObject
+    ///  result_ = Shambhala.MerchantOfflineServer.Tool.Adapters.AbpJson.ToJson
+    /// </summary>
+    public class AbpJson
+    {
+        public static T ToJson<T>(string responseStr, JsonSerializerSettings settings)
+        {
+            var r = Newtonsoft.Json.JsonConvert.DeserializeObject<AjaxResponse<T>>(responseStr, settings);
+
+            if (r.Success)
+                return r.Result;
+            else
+            {
+                throw new Exception(r.Error.Message);
+            }
+        }
+
+
+        public static T ToJson<T>(JsonTextReader responseStr, JsonSerializerSettings settings)
+        {
+            var serializer = Newtonsoft.Json.JsonSerializer.Create(settings);
+            var r = serializer.Deserialize<AjaxResponse<T>>(responseStr);
+
+            if (r.Success)
+                return r.Result;
+            else
+            {
+                throw new Exception(r.Error.Message);
+            }
+
+        }
+    }
+}
---
+++ AbpClient/IClient_Impl.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API
+{
+    public partial interface IClient
+    {
+        void SetToken(string token);
+    }
+
+    public partial class Client
+    {
+        string token;
+        public void SetToken(string token)
+        {
+            this.token = token;
+        }
+        partial void PrepareRequest(System.Net.Http.HttpClient client, System.Net.Http.HttpRequestMessage request, string url)
+        {
+            //  request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        }
+
+        partial void ProcessResponse(System.Net.Http.HttpClient client, System.Net.Http.HttpResponseMessage response)
+        {
+            var status_ = ((int)response.StatusCode).ToString();
+            if (status_ != "200" && status_ != "204")
+            {
+                var responseData_ = response.Content == null ? null :  response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                var typedBody = AbpJson.ToJson<AjaxResponseBase>(responseData_, JsonSerializerSettings);
+            }
+        }
+    }
+
+    public class ClientImpl : Client
+    {
+        public ClientImpl(string baseUrl) : base(baseUrl)
+        {
+        }
+
+        protected override async System.Threading.Tasks.Task<ObjectResponseResult<T>> ReadObjectResponseAsync<T>(System.Net.Http.HttpResponseMessage response, System.Collections.Generic.IReadOnlyDictionary<string, System.Collections.Generic.IEnumerable<string>> headers)
+        {
+            if (response == null || response.Content == null)
+            {
+                return new ObjectResponseResult<T>(default(T), string.Empty);
+            }
+
+            if (ReadResponseAsString)
+            {
+                var responseText = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                try
+                {
+                    var typedBody = AbpJson.ToJson<T>(responseText, JsonSerializerSettings);
+                    return new ObjectResponseResult<T>(typedBody, responseText);
+                }
+                catch (Newtonsoft.Json.JsonException exception)
+                {
+                    var message = "Could not deserialize the response body string as " + typeof(T).FullName + ".";
+                    throw new ApiException(message, (int)response.StatusCode, responseText, headers, exception);
+                }
+            }
+            else
+            {
+                try
+                {
+                    using (var responseStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                    using (var streamReader = new System.IO.StreamReader(responseStream))
+                    using (var jsonTextReader = new Newtonsoft.Json.JsonTextReader(streamReader))
+                    {
+                        var typedBody = AbpJson.ToJson<T>(jsonTextReader, JsonSerializerSettings);
+                        return new ObjectResponseResult<T>(typedBody, string.Empty);
+                    }
+                }
+                catch (Newtonsoft.Json.JsonException exception)
+                {
+                    var message = "Could not deserialize the response body stream as " + typeof(T).FullName + ".";
+                    throw new ApiException(message, (int)response.StatusCode, string.Empty, headers, exception);
+                }
+            }
+        }
+    }
+}
